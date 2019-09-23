0ca891fe86d05fba07d0cd61494d39de5680c903
sluong4 <seyanaluong@gmail.com>
2019-09-23 19:57:40 -0400

    fixed display bug

--- bootstrap_Game/js/test.js
+++ bootstrap_Game/js/test.js
@@ -3,10 +3,10 @@ var request = new XMLHttpRequest();
 
 var input = document.getElementById("gameTitle");
 input.addEventListener("keyup", function(e) {
-    if(e.keyCode === 13) {
-        e.preventDefault();
-        document.getElementById("submitBtn").click();
-    }
+  if (e.keyCode === 13) {
+    e.preventDefault();
+    document.getElementById("submitBtn").click();
+  }
 });
 
 function getGameData() {
@@ -21,27 +21,19 @@ function getGameData() {
   request.onload = function() {
     var data = JSON.parse(this.response);
     if (request.status >= 200 && request.status < 400) {
-    //   document.getElementById("gameName").innerHTML = data.name;
       getGameTitle(data);
       getIMG(data);
       getGenre(data);
-    //   document.getElementById("gameDescription").innerHTML = data.description;
       getGameDescription(data);
       getMetacritic(data);
-    //   document.getElementById("gameReleaseDate").innerHTML =
-    //     "Release Date: " + data.released;
-    getReleaseDate(data);
+      getReleaseDate(data);
       getPlatforms(data);
       getReddit(data);
       getWebsite(data);
-    //   document.getElementById("gameWebsite").href = data.website;
-    //   document.getElementById("gameWebsite").innerHTML = data.website;
-    //   document.getElementById("gameReddit").href = data.reddit_url;
-    //   document.getElementById("gameReddit").innerHTML = data.reddit_name;
       getDeveloper(data);
       getPublisher(data);
       getESRB(data);
-      document.getElementById("gameDetails").style = "display:flex;";
+      document.getElementById("gameDetails").style = "display: flex;";
     } else if (request.status == 404) {
       alert("Error 404: Game cannot be found.");
     } else {
@@ -52,52 +44,59 @@ function getGameData() {
 }
 
 function getReddit(data) {
-    if(data.website) {
-        document.getElementById("gameReddit").href = data.reddit_url;
-        document.getElementById("gameReddit").innerHTML = data.reddit_name;
-    } else {
-        document.getElementById("gameReddit").style = "display:none;";
-    }
+  if (data.reddit_url) {
+    document.getElementById("gameReddit").href = data.reddit_url;
+    document.getElementById("gameReddit").innerHTML = data.reddit_name;
+    document.getElementById("gameReddit").style = "display: inline-block;";
+  } else {
+    document.getElementById("gameReddit").style = "display: none;";
+  }
 }
 
 function getWebsite(data) {
-    if(data.website) {
-        document.getElementById("gameWebsite").href = data.website;
-        document.getElementById("gameWebsite").innerHTML = data.website;
-    } else {
-        document.getElementById("gameWebsite").style = "display:none;";
-    }
+  if (data.website) {
+    document.getElementById("gameWebsite").href = data.website;
+    document.getElementById("gameWebsite").innerHTML = data.website;
+    document.getElementById("gameWebsite").style = "display: inline-block;";
+  } else {
+    document.getElementById("gameWebsite").style = "display: none;";
+  }
 }
 
 function getReleaseDate(data) {
-    if(data.released) {
-        document.getElementById("gameReleaseDate").innerHTML = "Release Date: " + data.released;
-    } else {
-        document.getElementById("gameReleaseDate").style = "display:none;";
-    }
+  if (data.released) {
+    document.getElementById("gameReleaseDate").innerHTML =
+      "Release Date: " + data.released;
+      document.getElementById("gameReleaseDate").style = "display: flex;";
+  } else {
+    document.getElementById("gameReleaseDate").style = "display: none;";
+  }
 }
 function getGameDescription(data) {
-    if(data.description) {
-        document.getElementById("gameDescription").innerHTML = data.description;
-    } else {
-        document.getElementById("gameDescription").style = "display:none;";
-    }
+  if (data.description) {
+    document.getElementById("gameDescription").innerHTML = data.description;
+    document.getElementById("gameDescription").style = "display: block;";
+  } else {
+    document.getElementById("gameDescription").style = "display: none;";
+  }
 }
 
 function getGameTitle(data) {
-    if(data.name) {
-        document.getElementById("gameName").innerHTML = data.name;
-    } else {
-        document.getElementById("gameDetails").style = "display:none;";
-    }
+  if (data.name) {
+    document.getElementById("gameName").innerHTML = data.name;
+    document.getElementById("gameDetails").style = "display: flex;";
+  } else {
+    document.getElementById("gameDetails").style = "display: none;";
+  }
 }
 
 function getESRB(data) {
   if (data.esrb_rating) {
     document.getElementById("ESRB").innerHTML =
       "ESRB Rating: " + data.esrb_rating.name;
+      document.getElementById("ESRB").style = "display: flex;";
   } else {
-    document.getElementById("ESRB").style = "display: none";
+    document.getElementById("ESRB").style = "display: none;";
   }
 }
 
@@ -110,12 +109,18 @@ function getIMG(data) {
 }
 
 function getGenre(data) {
-  var genreConcat = "";
-  data.genres.forEach(x => {
-    genreConcat = x.name + ", " + genreConcat;
-  });
-  genreConcat = genreConcat.substring(0, genreConcat.length - 2);
-  document.getElementById("gameGenre").innerHTML = "Genre: " + genreConcat;
+  if(data.genres) {
+    var genreConcat = "";
+    data.genres.forEach(x => {
+      genreConcat = x.name + ", " + genreConcat;
+    });
+    genreConcat = genreConcat.substring(0, genreConcat.length - 2);
+    document.getElementById("gameGenre").innerHTML = "Genre: " + genreConcat;
+    document.getElementById("gameGenre").style = "display: flex;";
+  } else {
+    document.getElementById("gameGenre").style = "display: none;";
+  }
+  
 }
 
 function getMetacritic(data) {
@@ -138,28 +143,49 @@ function getMetacritic(data) {
 }
 
 function getPlatforms(data) {
-  var platformConcat = "";
-  data.platforms.forEach(x => {
-    platformConcat = x.platform.name + ", " + platformConcat;
-  });
-  platformConcat = platformConcat.substring(0, platformConcat.length - 2);
-  document.getElementById("gamePlatform").innerHTML = "Platforms: " + platformConcat;
+  if(data.platforms){
+    var platformConcat = "";
+    data.platforms.forEach(x => {
+      platformConcat = x.platform.name + ", " + platformConcat;
+    });
+    platformConcat = platformConcat.substring(0, platformConcat.length - 2);
+    document.getElementById("gamePlatform").innerHTML =
+      "Platforms: " + platformConcat;
+      document.getElementById("gamePlatform").style = "display: flex;";
+  } else {
+    document.getElementById("gamePlatform").style = "display: none;";
+  }
+  
 }
 
 function getDeveloper(data) {
-  var devConcat = "";
-  data.developers.forEach(x => {
-    devConcat = x.name + ", " + devConcat;
-  });
-  devConcat = devConcat.substring(0, devConcat.length - 2);
-  document.getElementById("gameDeveloper").innerHTML = "Developer: " + devConcat;
+  if(data.developers) {
+    var devConcat = "";
+    data.developers.forEach(x => {
+      devConcat = x.name + ", " + devConcat;
+    });
+    devConcat = devConcat.substring(0, devConcat.length - 2);
+    document.getElementById("gameDeveloper").innerHTML =
+      "Developer: " + devConcat;
+      document.getElementById("gameDeveloper").style = "display: flex;";
+  } else {
+    document.getElementById("gameDeveloper").style = "display: none;";
+  }
+  
 }
 
 function getPublisher(data) {
-  var pubConcat = "";
-  data.publishers.forEach(x => {
-    pubConcat = x.name + ", " + pubConcat;
-  });
-  pubConcat = pubConcat.substring(0, pubConcat.length - 2);
-  document.getElementById("gamePublisher").innerHTML = "Publisher: " + pubConcat;
+  if(data.publishers) {
+    var pubConcat = "";
+    data.publishers.forEach(x => {
+      pubConcat = x.name + ", " + pubConcat;
+    });
+    pubConcat = pubConcat.substring(0, pubConcat.length - 2);
+    document.getElementById("gamePublisher").innerHTML =
+      "Publisher: " + pubConcat;
+      document.getElementById("gamePublisher").style = "display: flex;";
+  } else {
+    document.getElementById("gamePublisher").style = "display: none;";
+  }
+
 }
