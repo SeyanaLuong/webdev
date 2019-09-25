e4e4e484836cd9610169cc32ff6b9eec051003d0
SeyanaLuong <bungluongbn@yahoo.com>
2019-09-26 04:16:58 +0700

    refactoring, restructure, css update, bug fix - could not get game info with improper game name, fixed with game slug data

--- bootstrap_Game/js/card.js
+++ bootstrap_Game/js/card.js
@@ -2,14 +2,13 @@ const app = document.getElementById("root");
 const nextPage = "https://api.rawg.io/api/games?page=";
 const bySize = "&page_size=";
 const searchQuery = "&search=";
+const request = new XMLHttpRequest();
 var pages = 1;
 var pageSize = 15;
 var searchGame = "";
-var request = new XMLHttpRequest();
-
+var displayOnce = false;
 var getUrl = window.location.href;
 var url = new URL(getUrl);
-
 var page = url.searchParams.get("p");
 var page_size = url.searchParams.get("s");
 var search = url.searchParams.get("g");
@@ -19,33 +18,17 @@ if (page) {
   pageSize = page_size;
   searchGame = search;
   getGameCards(page, page_size, search);
+} else if (!displayOnce) {
+  getGameCards();
 }
 
 var input = document.getElementById("gameTitle");
-input.addEventListener("keyup", function(e) {
+input.addEventListener("keyup", function (e) {
   if (e.keyCode === 13) {
     e.preventDefault();
     document.getElementById("submitBtn").click();
   }
 });
-
-function disableBtns(data) {
-  var disPrev = document.getElementById("prevBtn");
-  var disNext = document.getElementById("nextBtn");
-  if (data.previous) {
-    disPrev.removeAttribute("disabled");
-  } else {
-    disPrev.setAttribute("disabled", "");
-  }
-  if (data.next) {
-    disNext.removeAttribute("disabled");
-  } else {
-    disNext.setAttribute("disabled", "");
-  }
-  if (data.next) {
-    document.getElementById("nextBtn").removeAttribute("disabled");
-  }
-}
 
 function getGameCards(pagesNo, page_size, search) {
   if (!pagesNo) {
@@ -65,14 +48,14 @@ function getGameCards(pagesNo, page_size, search) {
   request.open(
     "GET",
     nextPage +
-      (pagesNo || pages || 1) +
-      bySize +
-      (page_size || pageSize) +
-      searchQuery +
-      (search || gameName),
+    (pagesNo || pages || 1) +
+    bySize +
+    (page_size || pageSize) +
+    searchQuery +
+    (search || gameName),
     true
   );
-  request.onload = function() {
+  request.onload = function () {
     var data = JSON.parse(this.response);
     if (request.status >= 200 && request.status < 400) {
       data.results.forEach(game => {
@@ -89,16 +72,7 @@ function getGameCards(pagesNo, page_size, search) {
         const cardBody = document.createElement("div");
         cardBody.setAttribute("class", "card-body");
 
-        var parseName = game.name;
-        console.log(parseName);
-        parseName = parseName
-          .toLowerCase()
-          .replace(/'/g, "")
-          .replace(/-/g, "")
-          .replace(/[^a-zA-Z0-9]/g, "-");
-        parseName = parseName.replace(/--/g, "-");
-        console.log(parseName);
-
+        const parseName = game.slug;
         card.onclick = function moreInfo() {
           window.location = `gameInfo.html?q=${parseName}&p=${pages ||
             pagesNo}&s=${pageSize || page_size}&g=${gameName || search || ""}`;
@@ -130,63 +104,4 @@ function toNextPage() {
 function toPrevPage() {
   pages--;
   getGameCards(pages, pageSize, searchGame);
-}
-
-function getImageCard(game) {
-  const img = document.createElement("img");
-  img.setAttribute("class", "card-img-top");
-  if (game.background_image) {
-    img.setAttribute("src", game.background_image);
-  } else {
-    img.setAttribute("src", "");
-  }
-  return img;
-}
-
-function getPlatformCard(game) {
-  var platformConcat = "";
-  game.platforms.forEach(x => {
-    platformConcat = x.platform.name + ", " + platformConcat;
-  });
-  platformConcat = platformConcat.substring(0, platformConcat.length - 2);
-  const platformName = document.createElement("p");
-  platformName.setAttribute("class", "card-text");
-  platformName.textContent = "Platform: " + platformConcat;
-  return platformName;
-}
-
-function getGenreCard(game) {
-  var genreConcat = "";
-  game.genres.forEach(x => {
-    genreConcat = x.name + ", " + genreConcat;
-  });
-  genreConcat = genreConcat.substring(0, genreConcat.length - 2);
-  const genreName = document.createElement("p");
-  genreName.setAttribute("class", "card-text");
-  genreName.textContent = "Genre: " + genreConcat;
-  return genreName;
-}
-
-function getReleaseDateCard(game) {
-  const releaseDate = document.createElement("p");
-  releaseDate.setAttribute("class", "card-text");
-  releaseDate.textContent = "Release Date: " + game.released;
-  return releaseDate;
-}
-
-function getScreenShotsCard(game) {
-  const imgGallery = document.createElement("div");
-  imgGallery.setAttribute("class", "img-gallery");
-
-  for (var i = 1; i < 3; i++) {
-    var imgList = document.createElement("img");
-    if (game.short_screenshots[i]) {
-      var images = game.short_screenshots[i].image;
-    } else {
-      var images = "";
-    }
-    imgList.setAttribute("src", images);
-    imgGallery.appendChild(imgList);
-  }
-  return imgGallery;
 }
--- bootstrap_Game/js/script.js
+++ bootstrap_Game/js/script.js
@@ -1,10 +1,8 @@
 const api = "https://api.rawg.io/api/games/";
 const request = new XMLHttpRequest();
-
 var getUrl = window.location.href;
 var url = new URL(getUrl);
 var gameTitle = url.searchParams.get("q");
-
 var page = url.searchParams.get("p");
 var page_size = url.searchParams.get("s");
 var search = url.searchParams.get("g");
@@ -16,7 +14,7 @@ function backButton() {
 
 function getGameData() {
   request.open("GET", api + gameTitle, true);
-  request.onload = function() {
+  request.onload = function () {
     var data = JSON.parse(this.response);
     if (request.status >= 200 && request.status < 400) {
       getGameTitle(data);
@@ -39,148 +37,4 @@ function getGameData() {
     }
   };
   request.send();
-}
-
-function getReddit(data) {
-  if (data.reddit_url) {
-    document.getElementById("gameReddit").href = data.reddit_url;
-    document.getElementById("gameReddit").innerHTML = data.reddit_name;
-    document.getElementById("gameReddit").style = "display: inline-block;";
-  } else {
-    document.getElementById("gameReddit").style = "display: none;";
-  }
-}
-
-function getWebsite(data) {
-  if (data.website) {
-    document.getElementById("gameWebsite").href = data.website;
-    document.getElementById("gameWebsite").innerHTML = data.website;
-    document.getElementById("gameWebsite").style = "display: inline-block;";
-  } else {
-    document.getElementById("gameWebsite").style = "display: none;";
-  }
-}
-
-function getReleaseDate(data) {
-  if (data.released) {
-    document.getElementById("gameReleaseDate").innerHTML =
-      "Release Date: " + data.released;
-    document.getElementById("gameReleaseDate").style = "display: flex;";
-  } else {
-    document.getElementById("gameReleaseDate").style = "display: none;";
-  }
-}
-
-function getGameDescription(data) {
-  if (data.description) {
-    document.getElementById("gameDescription").innerHTML = data.description;
-    document.getElementById("gameDescription").style = "display: block;";
-  } else {
-    document.getElementById("gameDescription").style = "display: none;";
-  }
-}
-
-function getGameTitle(data) {
-  if (data.name) {
-    document.getElementById("gameName").innerHTML = data.name;
-    document.getElementById("gameDetails").style = "display: flex;";
-  } else {
-    document.getElementById("gameDetails").style = "display: none;";
-  }
-}
-
-function getESRB(data) {
-  if (data.esrb_rating) {
-    document.getElementById("ESRB").innerHTML =
-      "ESRB Rating: " + data.esrb_rating.name;
-    document.getElementById("ESRB").style = "display: flex;";
-  } else {
-    document.getElementById("ESRB").style = "display: none;";
-  }
-}
-
-function getIMG(data) {
-  if (data.background_image) {
-    document.getElementById("gameIMG").src = data.background_image;
-  } else {
-    document.getElementById("gameIMG").src = "";
-  }
-}
-
-function getGenre(data) {
-  if (data.genres) {
-    var genreConcat = "";
-    data.genres.forEach(x => {
-      genreConcat = x.name + ", " + genreConcat;
-    });
-    genreConcat = genreConcat.substring(0, genreConcat.length - 2);
-    document.getElementById("gameGenre").innerHTML = "Genre: " + genreConcat;
-    document.getElementById("gameGenre").style = "display: flex;";
-  } else {
-    document.getElementById("gameGenre").style = "display: none;";
-  }
-}
-
-function getMetacritic(data) {
-  var metacriticColor;
-  var score = data.metacritic;
-  if (!score) {
-    metacriticColor = "#000000";
-  }
-  if (score > 61) {
-    metacriticColor = "#66cc33;";
-  } else if (score > 40) {
-    metacriticColor = "#ffcc33;";
-  } else {
-    metacriticColor = "#ff0000;";
-  }
-  document.getElementById("gameMetacritic").innerHTML = score;
-  document.getElementById("gameMetacritic").style =
-    "padding: 0px; text-align: center; color: #ffffff; background-color: " +
-    metacriticColor;
-}
-
-function getPlatforms(data) {
-  if (data.platforms) {
-    var platformConcat = "";
-    data.platforms.forEach(x => {
-      platformConcat = x.platform.name + ", " + platformConcat;
-    });
-    platformConcat = platformConcat.substring(0, platformConcat.length - 2);
-    document.getElementById("gamePlatform").innerHTML =
-      "Platforms: " + platformConcat;
-    document.getElementById("gamePlatform").style = "display: flex;";
-  } else {
-    document.getElementById("gamePlatform").style = "display: none;";
-  }
-}
-
-function getDeveloper(data) {
-  if (data.developers) {
-    var devConcat = "";
-    data.developers.forEach(x => {
-      devConcat = x.name + ", " + devConcat;
-    });
-    devConcat = devConcat.substring(0, devConcat.length - 2);
-    document.getElementById("gameDeveloper").innerHTML =
-      "Developer: " + devConcat;
-    document.getElementById("gameDeveloper").style = "display: flex;";
-  } else {
-    document.getElementById("gameDeveloper").style = "display: none;";
-  }
-}
-
-function getPublisher(data) {
-  if (data.publishers) {
-    var pubConcat = "";
-    data.publishers.forEach(x => {
-      pubConcat = x.name + ", " + pubConcat;
-    });
-    pubConcat = pubConcat.substring(0, pubConcat.length - 2);
-    document.getElementById("gamePublisher").innerHTML =
-      "Publisher: " + pubConcat;
-    document.getElementById("gamePublisher").style = "display: flex;";
-  } else {
-    document.getElementById("gamePublisher").style = "display: none;";
-  }
 }
